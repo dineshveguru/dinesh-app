afd347a77fa22230916bd0319607aa58eac36ec4
dinesh <dineshveguru123@gmail.com>
2024-01-07 12:47:14 +0530

    frontend started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dinesh</h1>
+    <main className="flex flex-col justify-center align-center h-screen px-10 bg-cover bg-center">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-black-900 md:text-5xl lg:text-6xl">
+        Pradhan Mantri Ujjwala Yojana (PMUY)
+      </h1>
+      <div className="flex w-full mt-5">
+        <button className="btn">
+          <Link href="./users/apply">Apply Here</Link>
+        </button>
+        <div className="divider divider-horizontal"></div>
+        <button className="btn btn-outline">
+          <Link href="./users/status">Check your Application status</Link>
+        </button>
+      </div>
     </main>
   );
 }
---
+++ src/app/users/apply/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Apply = () => {
+  return (
+    <div>
+      <h1 className="mb-4 text-4xl">Check your application status</h1>
+    </div>
+  );
+};
+
+export default Apply;
